41e3d9d0562da6d18e463f037305613597ae54a6
John Stuby <johnstuby7@gmail.com>
2023-07-20 14:59:36 -0600

    Added in examples of using functions as values and defining functions inside of functions

--- Example-2-Javascript-Refresher/assets/scripts/app.js
+++ Example-2-Javascript-Refresher/assets/scripts/app.js
@@ -104,3 +104,29 @@
 // for (const hobby of hobbies) {
 //   console.log(hobby)
 // } 
+
+// Using Functions as values:
+// function handleTimeout() {
+//   console.log("Timed out!");
+// }
+
+// const handleTimeout2 = () => {
+//   console.log("Timed out ... again!");
+// };
+
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+//   console.log("More timing out...");
+// }, 4000);
+
+// Defining FUnctions inside functions:
+// function init() {
+//   function greet() {
+//     console.log(“Hi!“);
+//   }
+
+//   greet();
+// }
+
+// init();
