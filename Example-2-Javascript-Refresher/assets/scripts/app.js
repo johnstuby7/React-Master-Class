161250c75bcf4ba4dd4558469564cdaf7a691929
John Stuby <johnstuby7@gmail.com>
2023-07-20 14:30:58 -0600

    Added in basic examples of arrays, maping, destructuring, etc

--- Example-2-Javascript-Refresher/assets/scripts/app.js
+++ Example-2-Javascript-Refresher/assets/scripts/app.js
@@ -1,7 +1,7 @@
 // Import examples
 // import { apikey } from "./util";
-import apikey from "./util.js";
-console.log(apikey);
+// import apikey from "./util.js";
+// console.log(apikey);
 
 // arrow function example
 // export default (username, message) => {
@@ -10,11 +10,65 @@ console.log(apikey);
 // };
 
 // Function examples;
-function greetUser(userName, message = "Hello") {
+// function greetUser(userName, message = "Hello") {
   // console.log(userName);
   // console.log(message);
-  return "hi, I am " + userName + ". " + message;
+  // return "hi, I am " + userName + ". " + message;
 }
 
 // greetUser("max");
-greetUser("John", "Whats Up");
+// greetUser("John", "Whats Up");
+
+// object example:
+// const user = {
+//   name: 'John',
+//   age 33,
+// };
+
+// console.log(user.name)
+
+
+// more complex object example
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   greet() {
+//     console.log("Hi");
+//   }
+// }
+
+// const user1 = new User("bob", 55);
+// console.log(user1);
+
+// Arrays Example
+// const hobbies = ["Sports", "Cooking", "Reading"]
+// console.log(hobbies[0]);
+
+// To push a item to a array:
+// hobbies.push("WOrkign");
+// console.log(hobbies);
+
+// How to find the index of a certain value in the array:
+// hobbies.findIndex((item) => {
+//   return item === "Sports";
+// } )
+
+// Map example:
+// const editedHobbies = hobbies.map((item) => item + ";")
+// console.log(editedHobbies)
+
+// Array methods, the following examples adds the val: to the front of each number
+// function transformToObjects(numberArray) {
+//   return numberArray.map((num) => { 
+//     let obj = { val: num };
+//     console.log(obj);
+//     return obj;
+//   });
+// } 
+
+// Destructure example: remap/change a existing array
+const [firstName, lastName] = ["joseph", "kling"];
+console.log(firstName, lastName);
