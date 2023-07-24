f7970862dea3b0783ddd90ea8b17418fd428ef08
John Stuby <johnstuby7@gmail.com>
2023-07-24 10:49:19 -0600

    Example of a different way of using props

--- Example-3-Basics-Of-React/01-starting-setup/src/App.js
+++ Example-3-Basics-Of-React/01-starting-setup/src/App.js
@@ -25,11 +25,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
+      <ExpenseItem title={expenses[0]}></ExpenseItem>
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
