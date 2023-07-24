f6b76bf6d652406943c85f1a57a15e6a5dba24fe
John Stuby <johnstuby7@gmail.com>
2023-07-24 12:05:19 -0600

    Finished creating the card component

--- Example-3-Basics-Of-React/01-starting-setup/src/components/Card.js
+++ Example-3-Basics-Of-React/01-starting-setup/src/components/Card.js
@@ -1,6 +1,7 @@
 import "./Card.css";
-function Card() {
-  return <div className="Card"></div>;
+function Card(props) {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
 }
 
 export default Card;
