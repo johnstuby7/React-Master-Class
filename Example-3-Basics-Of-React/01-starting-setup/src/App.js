1a8927b015850415950941bc2be19a36b62093c0
John Stuby <johnstuby7@gmail.com>
2023-07-26 15:21:03 -0600

    Example: List of Expenses is now updated based on new entries, lines 30-35 in App.js for logic

--- Example-3-Basics-Of-React/01-starting-setup/src/App.js
+++ Example-3-Basics-Of-React/01-starting-setup/src/App.js
@@ -1,33 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2023, 12, 22),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2024, 11, 25) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2023, 5, 26),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2023, 1, 12),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2023, 12, 22),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2024, 11, 25) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2023, 5, 26),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2023, 1, 12),
+  },
+];
 
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  // Updating state based on older snapshot.
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expenses);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
