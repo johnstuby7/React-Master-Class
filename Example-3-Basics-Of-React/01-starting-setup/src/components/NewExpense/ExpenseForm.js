3354ae5111e08bfec88857f306343bd1e888f7b0
John Stuby <johnstuby7@gmail.com>
2023-07-26 11:06:49 -0600

    Two way data binding example, the input fields will now be value the value of the entered amount, ensuring that they are always up to date

--- Example-3-Basics-Of-React/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ Example-3-Basics-Of-React/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -52,7 +52,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -60,6 +64,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -69,6 +74,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2023-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
