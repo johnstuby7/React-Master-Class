b3d9f1aee6a6fab96a3ab1627a8a566d4998b6ce
John Stuby <johnstuby7@gmail.com>
2023-07-28 15:49:37 -0600

    Updated inputChangeHandler to get all of the previous input values for the form, this is done with the spread ... operator

--- Example-9-Practice-project-Investment-Calculator/01-starting-project/src/components/UserInput/UserInput.js
+++ Example-9-Practice-project-Investment-Calculator/01-starting-project/src/components/UserInput/UserInput.js
@@ -19,7 +19,11 @@ const UserInput = () => {
   };
 
   const inputChangeHandler = (input, value) => {
-    console.log(input, value);
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+      };
+    });
   };
   return (
     <form onSubmit={submitHandler} className="form">
