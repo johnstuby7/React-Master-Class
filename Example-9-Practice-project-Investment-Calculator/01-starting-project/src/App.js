8f5a501ccd94bc12b710398003860e405f3e631b
John Stuby <johnstuby7@gmail.com>
2023-07-28 17:25:04 -0600

    Added in conditional logic to ensure that field exist before trying to use them

--- Example-9-Practice-project-Investment-Calculator/01-starting-project/src/App.js
+++ Example-9-Practice-project-Investment-Calculator/01-starting-project/src/App.js
@@ -11,21 +11,24 @@ function App() {
 
   const yearlyData = [];
 
-  let currentSavings = +userInput["current-savings"];
-  const yearlyContribution = +userInput["yearly-contribution"];
-  const expectedReturn = +userInput["expected-return"] / 100;
-  const duration = +userInput["duration"];
+  if (userInput) {
+    let currentSavings = +userInput["current-savings"];
+    const yearlyContribution = +userInput["yearly-contribution"];
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
 
-  for (let i = 0; i < duration; i++) {
-    const yearlyInterest = currentSavings * expectedReturn;
-    currentSavings += yearlyInterest + yearlyContribution;
-    yearlyData.push({
-      year: i + 1,
-      yearlyInterest: yearlyInterest,
-      savingsEndOfYear: currentSavings,
-      yearlyContribution: yearlyContribution,
-    });
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
   }
+
   return (
     <div>
       <Header />
