6775873c81cc00544bc87fc6e1474b64bf4fdd37
John Stuby <johnstuby7@gmail.com>
2023-07-28 17:23:15 -0600

    Updated logic so that rerunning yearlyData logic whenever data is changed

--- Example-9-Practice-project-Investment-Calculator/01-starting-project/src/App.js
+++ Example-9-Practice-project-Investment-Calculator/01-starting-project/src/App.js
@@ -4,28 +4,28 @@ import ResultsTable from "./components/ResultsTable/ResultsTable";
 import UserInput from "./components/UserInput/UserInput";
 
 function App() {
-  const [results, setResults] = useState(null);
+  const [userInput, setUserInput] = useState(null);
   const calculateHandler = (userInput) => {
-    const yearlyData = [];
-
-    let currentSavings = +userInput["current-savings"];
-    const yearlyContribution = +userInput["yearly-contribution"];
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
-
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
-      });
-    }
-
-    setResults(yearlyData);
+    setUserInput(userInput);
   };
+
+  const yearlyData = [];
+
+  let currentSavings = +userInput["current-savings"];
+  const yearlyContribution = +userInput["yearly-contribution"];
+  const expectedReturn = +userInput["expected-return"] / 100;
+  const duration = +userInput["duration"];
+
+  for (let i = 0; i < duration; i++) {
+    const yearlyInterest = currentSavings * expectedReturn;
+    currentSavings += yearlyInterest + yearlyContribution;
+    yearlyData.push({
+      year: i + 1,
+      yearlyInterest: yearlyInterest,
+      savingsEndOfYear: currentSavings,
+      yearlyContribution: yearlyContribution,
+    });
+  }
   return (
     <div>
       <Header />
