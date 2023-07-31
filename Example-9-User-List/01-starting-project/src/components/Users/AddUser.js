a8ad437a780d71f3639bbdb7431668a037927898
John Stuby <johnstuby7@gmail.com>
2023-07-31 12:14:26 -0600

    Updated Adduser component to accept and store user input values, added in comments for good explanation of the logic

--- Example-9-User-List/01-starting-project/src/components/Users/AddUser.js
+++ Example-9-User-List/01-starting-project/src/components/Users/AddUser.js
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  // this is called array destructuring: useState always returns two elements, and with this syntax we store them in seperate
+  // constants EntereduserNamea and setenteredUsername
+  // enteredUsername contains the latest snapshot of the field
+  // setEnteredUsername contains a function that changes the state of the enteredUsername, causing a rerender cycle
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+  };
+
+  const usernameChangeHandler = (event) => {
+    // event.target.value is the current value of the field
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="text" />
-        <button type="submit">Add User </button>
+        <input id="age" type="text" onChange={ageChangeHandler} />
+        <Button type="submit">Add User </Button>
       </form>
     </Card>
   );
