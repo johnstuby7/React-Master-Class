61d099189ae3bbcff19bf2b584bdff46910d3023
John Stuby <johnstuby7@gmail.com>
2023-07-31 11:27:39 -0600

    finished card component and added it to addUser. Has example of how to pass all the css values needed from one component to another

--- Example-9-User-List/01-starting-project/src/components/UI/Card.js
+++ Example-9-User-List/01-starting-project/src/components/UI/Card.js
@@ -1,9 +1,11 @@
-import react from "React";
 import classes from "./Card.module.css";
 
 // props.children will pass the content of the form into the card component
+// To dynamically pass in the various styles for the file we are using the `${}`
 const Card = (props) => {
-  return <div className={classes.card}>{props.children}</div>;
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  );
 };
 
 export default Card;
