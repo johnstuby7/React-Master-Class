781c92e1cc03993699fc196f76274e0d4e23e795
John Stuby <johnstuby7@gmail.com>
2023-08-01 14:51:42 -0600

    Made it so the modal pop up passes data between components, and is styled correctly

---
+++ Example-13-Food-Order-App/01-starting-project/src/components/UI/Modal.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+const Modal = (props) => {
+  return (
+    <Fragment>
+      <Backdrop />
+      <ModalOverlay>{props.children}</ModalOverlay>
+    </Fragment>
+  );
+};
+
+export default Modal;
