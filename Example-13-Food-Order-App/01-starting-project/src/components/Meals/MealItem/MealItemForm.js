9b090bee5d2f13b8bae39e24c7bb32c06ff3665e
John Stuby <johnstuby7@gmail.com>
2023-08-01 16:18:26 -0600

    Add to cart handler is now hooked up correctly and works as expected

--- Example-13-Food-Order-App/01-starting-project/src/components/Meals/MealItem/MealItemForm.js
+++ Example-13-Food-Order-App/01-starting-project/src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,29 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
   const amountInputRef = useRef();
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    // +enteredAmount will convert string to number
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
   };
 
   return (
@@ -24,6 +41,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
