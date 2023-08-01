62e280749a775a8264096ed67327eb85189da23b
John Stuby <johnstuby7@gmail.com>
2023-08-01 10:31:56 -0600

    Updated logic to now use the authcontext logic

--- Example-12-Use-Reducer-Example/06-usereducer-starting-project/src/App.js
+++ Example-12-Use-Reducer-Example/06-usereducer-starting-project/src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  const ctx = useContext(AuthContext);
   return (
     <React.Fragment>
       <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
     </React.Fragment>
   );
