0f3dc507ba61b19b7652827811c9bc1c785e3689
John Stuby <johnstuby7@gmail.com>
2023-08-01 15:37:02 -0600

    began to use the context for the cart, it now shows the default starting value, added in a Reduce method example to take a array and turn it into a number

--- Example-13-Food-Order-App/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ Example-13-Food-Order-App/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  // method allows us to transform a array into a single value, in this case a number
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
