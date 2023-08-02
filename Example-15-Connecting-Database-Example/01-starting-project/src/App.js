00f78edfc48f633f3a07b2244e1140169b35df01
John Stuby <johnstuby7@gmail.com>
2023-08-02 14:50:18 -0600

    Added in a try catch to the logic to be able to catch errors and display them correctly to the user

--- Example-15-Connecting-Database-Example/01-starting-project/src/App.js
+++ Example-15-Connecting-Database-Example/01-starting-project/src/App.js
@@ -6,22 +6,32 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setisLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setisLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
-    setisLoading(false);
+      if (!response.ok) {
+        throw new Error("Something went Wrong!");
+      }
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setisLoading(false);
+    } catch {
+      setError(error.message);
+    }
   }
 
   return (
