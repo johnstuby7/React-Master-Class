17dcae7affbc0bd68ccf48a88286f0d0dd2a8c52
John Stuby <johnstuby7@gmail.com>
2023-08-02 13:37:33 -0600

    Wrapped the DemoOutput in a React.memo function so that the demoOutput is only called when a change in the props has occured

--- Example-14-Optimization-Examples/01-starting-setup/src/components/Demo/DemoOutput.js
+++ Example-14-Optimization-Examples/01-starting-setup/src/components/Demo/DemoOutput.js
@@ -4,4 +4,7 @@ const DemoOutput = (props) => {
   return <p>{props.show ? "This is New" : ""}</p>;
 };
 
-export default DemoOutput;
+// This is one way to help prevent unnecessary re-evaluations
+// React.mom should look at a component as a argument, look at the props, compare the previous with new values of props, and only
+// if a change has occured, then the demoOutput will be run, otherwise it will be skipped"
+export default React.memo(DemoOutput);
