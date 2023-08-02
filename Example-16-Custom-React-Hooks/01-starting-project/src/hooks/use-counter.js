367c93bb9fcf64d548fdf2c7d1608636cb7ef24d
John Stuby <johnstuby7@gmail.com>
2023-08-02 16:35:33 -0600

    Finished updating the counters to both be reusing the custom counter hook

--- Example-16-Custom-React-Hooks/01-starting-project/src/hooks/use-counter.js
+++ Example-16-Custom-React-Hooks/01-starting-project/src/hooks/use-counter.js
@@ -1,16 +1,20 @@
 import { useState, useEffect } from "react";
 
 // Naming conventions for hooks means that name has to begin with use
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 };
