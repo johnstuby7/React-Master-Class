ebb6a35d50419f0a83f4d8d678edf1b9e8dc6e20
John Stuby <johnstuby7@gmail.com>
2023-08-02 09:05:18 -0600

    Updated cart to only show Order button if the cart has items, added in new component for cartItem, hasnt been hooked up yet

--- Example-13-Food-Order-App/01-starting-project/src/components/Cart/Cart.js
+++ Example-13-Food-Order-App/01-starting-project/src/components/Cart/Cart.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import cartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const cartCtx = useContext(cartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -16,13 +23,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
