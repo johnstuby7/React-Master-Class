0124204ea95519a62ff4da1e7d70abd4daa9c6e6
John Stuby <johnstuby7@gmail.com>
2023-08-03 10:16:38 -0600

    CUSTOM HOOK EXAMPLE: created a use-http custom hook to handle loading, error and sendrequest data

--- Example-17-Custom-React-Hooks-Realistic-Example/04-onwards-to-a-more-realistic-example/src/App.js
+++ Example-17-Custom-React-Hooks-Realistic-Example/04-onwards-to-a-more-realistic-example/src/App.js
@@ -4,37 +4,6 @@ import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [tasks, setTasks] = useState([]);
-
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-25cf6-default-rtdb.firebaseio.com/"
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
-
   useEffect(() => {
     fetchTasks();
   }, []);
