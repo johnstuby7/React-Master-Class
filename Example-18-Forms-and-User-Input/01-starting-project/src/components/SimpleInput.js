aa2a944c79c02795d1041200a3e96a382f6108e4
John Stuby <johnstuby7@gmail.com>
2023-08-03 11:27:09 -0600

    FORM SUBMISSION EXAMPLE: Refs: Added in a different example of how to handle form submission using refs

--- Example-18-Forms-and-User-Input/01-starting-project/src/components/SimpleInput.js
+++ Example-18-Forms-and-User-Input/01-starting-project/src/components/SimpleInput.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
 
   const nameInputChangeHandler = (event) => {
@@ -11,13 +12,20 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
