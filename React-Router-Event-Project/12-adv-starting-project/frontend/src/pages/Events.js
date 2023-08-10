041905d1583dc5f30f3f6e52d9bc640678f770a3
John Stuby <johnstuby7@gmail.com>
2023-08-10 16:42:08 -0600

    updated else logic in events

--- React-Router-Event-Project/12-adv-starting-project/frontend/src/pages/Events.js
+++ React-Router-Event-Project/12-adv-starting-project/frontend/src/pages/Events.js
@@ -3,9 +3,10 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
-  return <EventsList />;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
@@ -15,7 +16,6 @@ export async function loader() {
 
   if (!response.ok) {
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
