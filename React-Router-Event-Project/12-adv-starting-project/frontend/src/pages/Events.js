eed3e3fd309ec553e8534cad951a405cb6a5b3e3
John Stuby <johnstuby7@gmail.com>
2023-08-10 13:39:55 -0600

    added in dummy data for events, created a un ordered list to display said data

--- React-Router-Event-Project/12-adv-starting-project/frontend/src/pages/Events.js
+++ React-Router-Event-Project/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const DUMMY_EVNTS = [
+  {
+    id: "e1",
+    title: "Some event",
+  },
+  {
+    id: "e2",
+    title: "Another  event",
+  },
+];
 
 function EventsPage() {
-  return <h1>Events</h1>;
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {DUMMY_EVNTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;
