500d619216ad46249ea8834dee2ec166ed91ea5f
John Stuby <johnstuby7@gmail.com>
2023-08-10 13:47:31 -0600

    Updated eventsNavigation to use NavLink

--- React-Router-Event-Project/12-adv-starting-project/frontend/src/components/EventsNavigation.js
+++ React-Router-Event-Project/12-adv-starting-project/frontend/src/components/EventsNavigation.js
@@ -1,6 +1,7 @@
-import classes from "./EventsNavigation.module.css";
-
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+import classes from "./EventsNavigation.module.css";
 
 function EventsNavigation() {
   return (
@@ -8,10 +9,25 @@ function EventsNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <a href="/events">All Events</a>
+            <NavLink
+              to="/events"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              All Events
+            </NavLink>
           </li>
           <li>
-            <a href="/events/new">New Event</a>
+            <NavLink
+              to="/events/new"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              New Event
+            </NavLink>
           </li>
         </ul>
       </nav>
