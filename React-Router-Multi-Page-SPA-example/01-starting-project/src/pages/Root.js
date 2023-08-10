5b95bafbc70c6f4c2d34419b0066e8617440790a
John Stuby <johnstuby7@gmail.com>
2023-08-10 08:42:02 -0600

    Oultet example continuation

--- React-Router-Multi-Page-SPA-example/01-starting-project/src/pages/Root.js
+++ React-Router-Multi-Page-SPA-example/01-starting-project/src/pages/Root.js
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router-dom";
 
 function RootLayout() {
-  return <h1>Root Layout</h1>;
+  return (
+    <>
+      <h1>Root Layout</h1>; // Outlet is the marker where the child routes will
+      be rendered to
+      <Outlet />
+    </>
+  );
 }
 
 export default RootLayout;
