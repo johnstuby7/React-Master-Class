9015575c90b7b2ff19db4b994af0afde37a5842a
John Stuby <johnstuby7@gmail.com>
2023-08-10 08:31:28 -0600

    Basic link example between pages

--- React-Router-Multi-Page-SPA-example/01-starting-project/src/pages/Home.js
+++ React-Router-Multi-Page-SPA-example/01-starting-project/src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function HomePage() {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <a href="/products">The List of Products</a>.
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
