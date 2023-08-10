900683d561d23e048c310e78d94f547301ced4a0
John Stuby <johnstuby7@gmail.com>
2023-08-10 12:01:03 -0600

    EXAMPLE: dynamic route, modifying the url will allow you to see the new productDetail page in action

--- React-Router-Multi-Page-SPA-example/01-starting-project/src/pages/ProductDetail.js
+++ React-Router-Multi-Page-SPA-example/01-starting-project/src/pages/ProductDetail.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
 function ProductDetailPage() {
-  return <h1>Product Details</h1>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Product Details</h1>
+      <p>{params.productId}</p>
+    </>
+  );
 }
 
 export default ProductDetailPage;
--- React-Router-Multi-Page-SPA-example/01-starting-project/src/pages/Products.js
+++ React-Router-Multi-Page-SPA-example/01-starting-project/src/pages/Products.js
@@ -3,7 +3,7 @@ import React from "react";
 function ProductsPage() {
   return (
     <>
-      <h1>The Products Page</h1>;
+      <h1>The Products Page</h1>
       <ul>
         <li>Product 1</li>
         <li>Product 2</li>
