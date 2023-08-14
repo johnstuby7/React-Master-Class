cdbe892f84005fb46f337d6635d43f9f52d6f258
John Stuby <johnstuby7@gmail.com>
2023-08-14 09:42:34 -0600

    Example: adding in the auth action, first step in passing login information to the backend, iin this commit we have gotten the mode the user is in, email and password they have, next will be posting said info to the backend'

--- Example-20-Adding-Authentication-To-Apps/01-starting-project/01-starting-project/frontend/src/pages/Authentication.js
+++ Example-20-Adding-Authentication-To-Apps/01-starting-project/01-starting-project/frontend/src/pages/Authentication.js
@@ -1,7 +1,23 @@
-import AuthForm from '../components/AuthForm';
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  // Below is how we are able to access the mode that we are in, we
+  // assign the url search params and then are able to access the mode from it
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  const data = await request.formData();
+
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  fetch("http://localhost:8080/" + mode);
+}
