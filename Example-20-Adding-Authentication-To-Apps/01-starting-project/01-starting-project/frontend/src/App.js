28e61872846c69ccf4f86ceb7d275d4951ab1fdc
John Stuby <johnstuby7@gmail.com>
2023-08-14 11:05:52 -0600

    Updated event item/navigation to conditionally show things if the state is correct

--- Example-20-Adding-Authentication-To-Apps/01-starting-project/01-starting-project/frontend/src/App.js
+++ Example-20-Adding-Authentication-To-Apps/01-starting-project/01-starting-project/frontend/src/App.js
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
-    id="root",
+    id: "root",
     loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
