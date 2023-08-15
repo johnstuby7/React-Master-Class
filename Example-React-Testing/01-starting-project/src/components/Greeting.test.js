af7a1b951e6cc2d7682e22c842e53c6354fe9acf
John Stuby <johnstuby7@gmail.com>
2023-08-15 15:01:44 -0600

    Added in a couple example tests, multiple to react to the different text that appears when a button is clicked

--- Example-React-Testing/01-starting-project/src/components/Greeting.test.js
+++ Example-React-Testing/01-starting-project/src/components/Greeting.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
-  test("redners Hello World as a text", () => {
+  test("renders Hello World as a text", () => {
     // Arrange
     render(<Greeting />);
 
@@ -12,5 +13,37 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World", { exact: true });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders Its good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("Its good to see you");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders Changed if the button was clicked", () => {
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // assert
+    const outputElement = screen.getByText("Changed");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render Good to see you if the button was clicked", () => {
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // assert
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
