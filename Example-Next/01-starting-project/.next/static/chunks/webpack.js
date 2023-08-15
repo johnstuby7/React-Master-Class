570c0d42014c56b5d2d4c098968a12c20dfcce56
John Stuby <johnstuby7@gmail.com>
2023-08-15 12:08:59 -0600

    Added a link to the details page using the next/link Link import

--- Example-Next/01-starting-project/.next/static/chunks/webpack.js
+++ Example-Next/01-starting-project/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9b5a67f0d092180d"; }
+/******/ 		__webpack_require__.h = function() { return "9735b568634cd713"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Example-Next/01-starting-project/pages/news/index.js
+++ Example-Next/01-starting-project/pages/news/index.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1> The News Page</h1>;
+  return (
+    <Fragment>
+      <h1> The news Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJs is a great framework
+          </Link>
+        </li>
+        <li>More news</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
